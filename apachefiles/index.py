2f7ec086aa20bd58f93bb628746f53423a2d325a
helloraji <konda.rajsekhar@gmail.com>
2017-05-30 03:53:38 +0000

    First Commit

--- apachefiles/index.py
+++ apachefiles/index.py
@@ -8,7 +8,7 @@ print """\
 <h2>Hello World!</h2>
 <h2>This is first Auto Deployment</h2>
 <h2>How about this line?</h2>
-<h2>Deployment is Complete!!!</h2>
+<h2>Deployment is Complete at 10:53PM!!!</h2>
 </body>
 </html>
 """
